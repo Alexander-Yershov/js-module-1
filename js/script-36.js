ab6b2d479e1c78bccec081c07982f1a1dcb2813b
Alexander-Yershov <alex04398956@gmail.com>
2021-09-07 13:24:30 +0200

    завершает модуль №1

---
+++ js/script-36.js
@@ -0,0 +1,7 @@
+function checkForSpam(message) {
+  let result;
+  // Change code below this line
+result = message.toLowerCase().includes('spam') || message.toLowerCase().includes('sale')
+  // Change code above this line
+  return result;
+}
