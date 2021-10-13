c7fa9bcecf941ce58565af3e9356feb397487b05
Alexander-Yershov <alex04398956@gmail.com>
2021-10-13 12:06:04 +0200

    изменяет первый модуль

---
+++ js/script-13.js
@@ -0,0 +1,8 @@
+function isValidPassword(password) {
+  const SAVED_PASSWORD = 'jqueryismyjam';
+  // Change code below this line
+  const isMatch = password === 'jqueryismyjam';
+
+  // Change code above this line
+  return isMatch;
+}
---
+++ js/script-8.js
@@ -0,0 +1,9 @@
+function add(a, b, c){
+  const result = a + b + c;
+  console.log(`Addition result equals ${result}`);
+  // Change code above this line
+};
+
+add(15, 27, 10);
+add(10, 20, 30);
+add(5, 10, 15);
